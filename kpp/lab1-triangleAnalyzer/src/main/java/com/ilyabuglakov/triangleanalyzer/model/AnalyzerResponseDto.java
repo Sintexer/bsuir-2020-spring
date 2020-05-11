a2c8db6232079162b1d66fbd40e2802bd8f4e7a5
Илья Буглаков <Neonlightnight@gmail.com>
2020-05-11 21:48:27 +0300

    added ResponseDto class

--- kpp/lab1-triangleAnalyzer/src/main/java/com/ilyabuglakov/triangleanalyzer/model/AnalyzerResponseDto.java
+++ kpp/lab1-triangleAnalyzer/src/main/java/com/ilyabuglakov/triangleanalyzer/model/AnalyzerResponseDto.java
@@ -3,12 +3,47 @@ package com.ilyabuglakov.triangleanalyzer.model;
 import java.util.List;
 
 public class AnalyzerResponseDto {
-//    int totalRequestsCount;
-//    int uniqueRequestCount;
-//    int invalidRequestCount;
+    long totalRequestsCount;
+    long uniqueRequestCount;
+    long invalidRequestCount;
     List<TriangleAttributes> responseList;
 
-    public AnalyzerResponseDto(List<TriangleAttributes> responseList) {
+    public long getTotalRequestsCount() {
+        return totalRequestsCount;
+    }
+
+    public void setTotalRequestsCount(long totalRequestsCount) {
+        this.totalRequestsCount = totalRequestsCount;
+    }
+
+    public long getUniqueRequestCount() {
+        return uniqueRequestCount;
+    }
+
+    public void setUniqueRequestCount(long uniqueRequestCount) {
+        this.uniqueRequestCount = uniqueRequestCount;
+    }
+
+    public long getInvalidRequestCount() {
+        return invalidRequestCount;
+    }
+
+    public void setInvalidRequestCount(long invalidRequestCount) {
+        this.invalidRequestCount = invalidRequestCount;
+    }
+
+    public List<TriangleAttributes> getResponseList() {
+        return responseList;
+    }
+
+    public void setResponseList(List<TriangleAttributes> responseList) {
+        this.responseList = responseList;
+    }
+
+    public AnalyzerResponseDto(long totalRequestCount, long uniqueRequestCount, long invalidRequestCount, List<TriangleAttributes> responseList) {
+        this.totalRequestsCount = totalRequestCount;
+        this.uniqueRequestCount= uniqueRequestCount;
+        this.invalidRequestCount = invalidRequestCount;
         this.responseList = responseList;
     }
 }
